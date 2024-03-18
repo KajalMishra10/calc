bf0151ae7d12ee210a10471460534232dc24a140
Kajal <kajana@amdocs.com>
2024-03-18 22:58:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+
+  const handleCalculate = () => {
+    if (input.trim() === "") {
+      setResult("Error");
+    } else {
+      try {
+        const calculatedResult = eval(input);
+        if (calculatedResult === Infinity || isNaN(calculatedResult)) {
+          setResult("NaN");
+        } else {
+          setResult(calculatedResult);
+        }
+      } catch (error) {
+        setResult("Error");
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="input-output">
+        <input type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick("7")}>7</button>
+        <button onClick={() => handleButtonClick("8")}>8</button>
+        <button onClick={() => handleButtonClick("9")}>9</button>
+        <button onClick={() => handleButtonClick("+")}>+</button>
+        <button onClick={() => handleButtonClick("4")}>4</button>
+        <button onClick={() => handleButtonClick("5")}>5</button>
+        <button onClick={() => handleButtonClick("6")}>6</button>
+        <button onClick={() => handleButtonClick("-")}>-</button>
+        <button onClick={() => handleButtonClick("1")}>1</button>
+        <button onClick={() => handleButtonClick("2")}>2</button>
+        <button onClick={() => handleButtonClick("3")}>3</button>
+        <button onClick={() => handleButtonClick("*")}>*</button>
+        <button onClick={() => handleButtonClick("0")}>0</button>
+        <button onClick={() => handleButtonClick("/")}>/</button>
+        <button onClick={handleClear}>C</button>
+        <button onClick={handleCalculate}>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
